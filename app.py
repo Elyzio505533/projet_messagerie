783c6821c20087de0393a360c6cf95077f6c7ebb
Dayssem SASSI <sassidayssem@gmail.com>
2026-03-24 10:01:12 +0100

    :construction: création des chemins flask

--- app.py
+++ app.py
@@ -3,8 +3,20 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
+def connexion():
+    return render_template('connexion.html')
+
+@app.route('/inscription')
+def inscription():
+    return render_template('inscription.html')
+
+@app.route('/accueil')
 def accueil():
     return render_template('accueil.html')
+
+@app.route('/discussion')
+def discussion():
+    return render_template('discussion.html')
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
